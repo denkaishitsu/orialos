a99020184a41192ff0cb21d286c16011510e50e7
ta hi <denanokk@gmail.com>
2018-04-05 16:48:40 +0900

    default message change & index msg change

--- wnc-for-aws/routes/index.js
+++ wnc-for-aws/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Waton Conversation API' });
+  res.render('index', { title: 'Watson Conversation API on AWS' });
 });
 
 module.exports = router;
